93da8e54ce2e5372c401956030e752256905b60f
Med1El <m3d13l@gmail.com>
2025-10-17 11:39:50 +0100

    feat: ajout entité Produit

--- src/main/java/com/groupeX/pos/entity/Produit.java
+++ src/main/java/com/groupeX/pos/entity/Produit.java
@@ -1,4 +1,30 @@
 package com.groupeX.pos.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "produits")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Produit {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String reference;
+    private double prix;
+
+    private String description;
+    private boolean disponible;
+
+    private Categorie categorie;
+
 }
