704cda8e13b6c10de447c11f356ddc2228c7de88
HAMZA EL BOUDALI <hamzalbdl2000@gmail.com>
2025-10-17 12:06:00 +0100

    categorie entity

--- src/main/java/com/groupeX/pos/entity/Categorie.java
+++ src/main/java/com/groupeX/pos/entity/Categorie.java
@@ -1,4 +1,29 @@
 package com.groupeX.pos.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Categorie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    private String description;
+    
+    @OneToMany(mappedBy = "categorie", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Produit> produits;
 }
