64f378d3fe7e66d211cb4180bbf8fd62e8772691
dell <achrafghani2003@gmail.com>
2025-10-22 10:21:30 +0100

    feat: ajout relation entre Commande et Produit

--- src/main/java/com/groupeX/pos/entity/Commande.java
+++ src/main/java/com/groupeX/pos/entity/Commande.java
@@ -1,4 +1,24 @@
 package com.groupeX.pos.entity;
 
+import com.groupeX.pos.entity.Produit;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Commande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime date = LocalDateTime.now();
+
+    @ManyToMany
+    private List<Produit> produits;
+
+    private double total;
 }
